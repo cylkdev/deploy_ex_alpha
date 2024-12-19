be87599b2385bea17faad64c72b1222897b6a160
Kurt Hogarth <87607684+cylkdev@users.noreply.github.com>
2024-12-18 22:46:42 -0500

    add command to start ansible

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,19 +6,9 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.7"
     }
-
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "3.0.2"
-    }
   }
 }
 
 provider "aws" {
   region  = "us-west-1"
-}
-
-provider "docker" {
-  host     = "ssh://user@remote-host:22"
-  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
 }
